c304dc5308d47deec363ae448c8b9d3d8fa1eb95
23407 <23407je@hvhs.school.nz>
2026-02-10 14:35:34 +1300

    sdfgsdf

--- t06_createAliens.js
+++ t06_createAliens.js
@@ -10,42 +10,11 @@
 function setup() {
 	console.log("setup: ");
 createCanvas(1920, 1080,);
-world.gravity.y = 10;
+wallLH  = new Sprite(0, height/2, 8, height, 'k');
+ball_1 = new Sprite(width/2, height/2, 50, 'd');
+for (i = 0; i < 100; i++) {
 
-wallLH  = new Sprite(0, height/2, 10, height, 'k');
-
-wallLH.color = '#0000';
-
-wallRH  = new Sprite(0, 200, 40, 40, 'k');
-
-wallTop = new Sprite(0, 600, 40, 40, 'k');
-
-wallBot = new Sprite(0, 1000, 40, 40, 'k');
-
-
-//shapes
-shape = new Sprite(400, 400, 40, 40);
-shape.color = '#87CEEB';
-shape.rotationSpeed = 2;
-shape.vel.x = 2;
-
-circle = new Sprite(width/2, height/2, 50, 'd');
-
-circle.color = '#87CEEB';
-
-circle.vel.x = 2;
-
-circle.bounciness = 1;
-
-circle.friction = 0;
-
-circle.drag = 0;
-
-platform = new Sprite(700, 1040, 1920, 40, 'k');
-
-for (i = 0; i < ; i++) {
-
-  alien = new Sprite(500, 500, 60);
+  alien = new Sprite();
 
   alien.vel.x = 3;
 
@@ -54,7 +23,9 @@ for (i = 0; i < ; i++) {
   alien.bounciness = 1;
 
   alien.friction = 0;
+
 }
+//shapes
 }
 /*******************************************************/
 // draw()
