72ae4a684909e4d3cb7029b12164332bd8054b8c
23407 <23407je@hvhs.school.nz>
2026-02-12 10:23:35 +1300

    dfgdfg

--- t08_colliders.js
+++ t08_colliders.js
@@ -1,6 +1,6 @@
 /*******************************************************/
-// P5.play: t08_colliders
-// Work with colliders
+// P5.play: t07_groups
+// Create aliens and add to a group on mouse click
 // Written by ???
 /*******************************************************/
 	
@@ -9,14 +9,43 @@
 /*******************************************************/
 function setup() {
 	console.log("setup: ");
+cnv = new Canvas(1920, 1080);2
 
+wallLH  = new Sprite(0, height/2, 8, height, 'k');
+
+ball_1 = new Sprite(width/2, height/2, 50, 'd');
+
+alienGroup.collides(ball_1, func2Call);
+
+function func2Call(_ssss, _ball_1) {
+
+// Delete the alien which was hit
+
+_ssss.remove();
+}
+alienGroup = new Group();
+
+for (i = 0; i < 100; i++) {
+
+  alien = new Sprite();
+
+  alien.vel.x = 3;
+
+  alien.vel.y = 4;
+
+  alien.bounciness = 1;
+
+  alien.friction = 0;
+
+  alienGroup.add(alien);
+}
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background(255, 200, 200);
 }
 
 /*******************************************************/
