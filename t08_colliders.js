30c4950a8dabeef8aa96db2ae2d1a19f3bf83d3f
23407 <23407je@hvhs.school.nz>
2026-02-12 10:34:53 +1300

    sdfsfd

--- t08_colliders.js
+++ t08_colliders.js
@@ -13,6 +13,12 @@ cnv = new Canvas(1920, 1080);2
 
 wallLH  = new Sprite(0, height/2, 8, height, 'k');
 
+wallRH  = new Sprite(0, 0, 40, 1920, 'k');
+
+wallTop = new Sprite(0, 0, 1920, 40, 'k');
+
+wallBot = new Sprite(0, 1080, 1920, 40, 'k');
+
 ball_1 = new Sprite(width/10, height/2, 20, 'd');
 ball_1.vel.x = 10;
 alienGroup = new Group();
